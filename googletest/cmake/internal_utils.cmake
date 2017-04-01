5d948bce2c0e59d9e9b92bd2f95eaace6712fc8a
Brandon Kohn <blkohn@hotmail.com>
2017-03-31 20:14:01 -0400

    Added macro to force msvc runtime to a specified value.

--- googletest/cmake/internal_utils.cmake
+++ googletest/cmake/internal_utils.cmake
@@ -22,34 +22,55 @@ endif (POLICY CMP0054)
 # update variables in the function scope.
 macro(fix_default_compiler_settings_)
   if (MSVC)
-    # For MSVC, CMake sets certain flags to defaults we want to override.
-    # This replacement code is taken from sample in the CMake Wiki at
+    if(NOT "${FORCE_MSVC_RUNTIME}" STREQUAL "")
+      message(STATUS "Calling replace loop.")
+      # Set compiler options.
+      message(STATUS "MSVC: Forcing runtime to ${FORCE_MSVC_RUNTIME}.")
+      foreach(variable CMAKE_C_FLAGS_DEBUG
+          CMAKE_C_FLAGS_MINSIZEREL
+          CMAKE_C_FLAGS_RELEASE
+          CMAKE_C_FLAGS_RELWITHDEBINFO
+          CMAKE_CXX_FLAGS_DEBUG
+          CMAKE_CXX_FLAGS_MINSIZEREL
+          CMAKE_CXX_FLAGS_RELEASE
+          CMAKE_CXX_FLAGS_RELWITHDEBINFO)
+        message(STATUS "Checking ${variable}")
+        #if(variable MATCHES "/M[TD]d?")
+            message(STATUS "Replacing ${${variable}}")
+            string(REGEX REPLACE "/M[TD]d?" "${FORCE_MSVC_RUNTIME}" ${variable} "${${variable}}")
+            message(STATUS "with      ${${variable}}")
+        #endif()
+      endforeach()
+    else()
+      # For MSVC, CMake sets certain flags to defaults we want to override.
+      # This replacement code is taken from sample in the CMake Wiki at
     # https://gitlab.kitware.com/cmake/community/wikis/FAQ#dynamic-replace.
-    foreach (flag_var
+      foreach (flag_var
              CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE
              CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO
              CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
              CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
-      if (NOT BUILD_SHARED_LIBS AND NOT gtest_force_shared_crt AND "${FORCE_MSVC_RUNTIME}" STREQUAL "")
-        # When Google Test is built as a shared library, it should also use
-        # shared runtime libraries.  Otherwise, it may end up with multiple
-        # copies of runtime library data in different modules, resulting in
-        # hard-to-find crashes. When it is built as a static library, it is
-        # preferable to use CRT as static libraries, as we don't have to rely
-        # on CRT DLLs being available. CMake always defaults to using shared
-        # CRT libraries, so we override that default here.
-        string(REPLACE "/MD" "-MT" ${flag_var} "${${flag_var}}")
-      endif()
+        if (NOT BUILD_SHARED_LIBS AND NOT gtest_force_shared_crt AND "${FORCE_MSVC_RUNTIME}" STREQUAL "")
+          # When Google Test is built as a shared library, it should also use
+          # shared runtime libraries.  Otherwise, it may end up with multiple
+          # copies of runtime library data in different modules, resulting in
+          # hard-to-find crashes. When it is built as a static library, it is
+          # preferable to use CRT as static libraries, as we don't have to rely
+          # on CRT DLLs being available. CMake always defaults to using shared
+          # CRT libraries, so we override that default here.
+          string(REPLACE "/MD" "-MT" ${flag_var} "${${flag_var}}")
+        endif()
 
-      # We prefer more strict warning checking for building Google Test.
-      # Replaces /W3 with /W4 in defaults.
-      string(REPLACE "/W3" "/W4" ${flag_var} "${${flag_var}}")
+        # We prefer more strict warning checking for building Google Test.
+        # Replaces /W3 with /W4 in defaults.
+        string(REPLACE "/W3" "/W4" ${flag_var} "${${flag_var}}")
 
       # Prevent D9025 warning for targets that have exception handling
       # turned off (/EHs-c- flag). Where required, exceptions are explicitly
       # re-enabled using the cxx_exception_flags variable.
       string(REPLACE "/EHsc" "" ${flag_var} "${${flag_var}}")
-    endforeach()
+      endforeach()
+    endif()
   endif()
 endmacro()
 
@@ -304,9 +325,9 @@ function(py_test name)
       endif (CMAKE_CONFIGURATION_TYPES)
     else()
       # ${CMAKE_CURRENT_BINARY_DIR} is known at configuration time, so we can
-      # directly bind it from cmake. ${CTEST_CONFIGURATION_TYPE} is known
-      # only at ctest runtime (by calling ctest -c <Configuration>), so
-      # we have to escape $ to delay variable substitution here.
+    # directly bind it from cmake. ${CTEST_CONFIGURATION_TYPE} is known
+    # only at ctest runtime (by calling ctest -c <Configuration>), so
+    # we have to escape $ to delay variable substitution here.
       if (WIN32 OR MINGW)
         add_test(NAME ${name}
           COMMAND powershell -Command ${CMAKE_CURRENT_BINARY_DIR}/RunTest.ps1
@@ -314,7 +335,7 @@ function(py_test name)
             --build_dir=${CMAKE_CURRENT_BINARY_DIR}/\${CTEST_CONFIGURATION_TYPE} ${ARGN})
       else()
         add_test(NAME ${name}
-          COMMAND ${PYTHON_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/test/${name}.py
+        COMMAND ${PYTHON_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/test/${name}.py
             --build_dir=${CMAKE_CURRENT_BINARY_DIR}/\${CTEST_CONFIGURATION_TYPE} ${ARGN})
       endif()
     endif()
